7ce1a77e943f7802f0771259af8570d26692ef06
kikim <85283385+gs-kikim@users.noreply.github.com>
2022-04-18 23:07:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
 terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+  required_version = ">= 0.12, < 0.13"
 }
 
 provider "aws" {
   region = "us-east-2"
+
+  # 2.x 버전의 AWS 공급자 허용
+  version = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
+  ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
