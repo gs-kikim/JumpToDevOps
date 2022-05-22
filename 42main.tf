7c3eaf20d6d7b17864faf3fedb9209ecafa97ef3
kikim <85283385+gs-kikim@users.noreply.github.com>
2022-05-22 22:12:04 +0900

    Update 42main.tf

--- 42main.tf
+++ 42main.tf
@@ -1,16 +1,21 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
-}
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 
-provider "aws" {
-  region = "us-east-2"
+  cloud {
+    organization = "example-teraform"
 
-  # 2.x 버전의 AWS 공급자 허용
-  version = "~> 2.0"
-}
-
-terraform {
-  backend "s3" {
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+   backend "s3" {
 
     # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
     # manually, uncomment and fill in the config below.
@@ -24,8 +29,15 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-2"
+
+  # 2.x 버전의 AWS 공급자 허용
+  version = "~> 2.0"
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           ="ami-0c55b159cbfafe1f0"
 
   instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
 
